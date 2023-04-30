babc40dc59e88a60428ae71c6958cd994a91ac5e
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-05-01 00:12:47 +0530

    ✅ added js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // javascript starts here
 
 // TODO: select all the elements on which we have to add listener
+const button = document.querySelector(".btn-search");
 
 // TODO: add dark mode toggler code
 
